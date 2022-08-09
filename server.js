474f2ef821c71299937b1aa16019c339d489a8f0
Alexis Rangel <alexis.rangel@codster.io>
2022-08-09 23:23:10 +0000

    server.js edited online with Bitbucket

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var path = require('path');
-//puerto
+//puerto de escucha
 const port = process.env.PORT ||8080;
 //const compression = require('compression')
 
