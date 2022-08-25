819e1c7e99a411ed86fe2b05e2f173c2f88d1913
CATAAA233 <carlosdanielcatalan1o@gmail.com>
2022-08-25 16:42:36 -0700

    adding array to component ts

--- src/app/components/featured-users/featured-users.component.ts
+++ src/app/components/featured-users/featured-users.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedUsersComponent implements OnInit {
 
+  userData =[
+    {
+      profileImage:'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg',
+    },
+    {
+      profileImage:'https://images.pexels.com/photos/2726111/pexels-photo-2726111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      profileImage:'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      profileImage:'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg',
+    },
+    {
+      profileImage:'https://images.pexels.com/photos/2726111/pexels-photo-2726111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      profileImage:'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
