fb73c532f085f9199d391bb73b6aa9dd79560390
CATAAA233 <carlosdanielcatalan1o@gmail.com>
2022-08-26 11:52:04 -0700

    limited users

--- src/app/components/featured-users/featured-users.component.ts
+++ src/app/components/featured-users/featured-users.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+interface UserDataType{
+  profileImage:string
+}
 @Component({
   selector: 'featured-users',
   templateUrl: './featured-users.component.html',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedUsersComponent implements OnInit {
 
-  userData =[
+  userData:UserDataType[] =[
     {
       profileImage:'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg',
     },
@@ -22,6 +25,12 @@ export class FeaturedUsersComponent implements OnInit {
     },
     {
       profileImage:'https://images.pexels.com/photos/2726111/pexels-photo-2726111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      profileImage:'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      profileImage:'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
     {
       profileImage:'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
@@ -29,6 +38,9 @@ export class FeaturedUsersComponent implements OnInit {
   ]
   constructor() { }
 
+  trackByFn(index:number, userData:UserDataType):number{
+    return index;
+  }
   ngOnInit(): void {
   }
 
