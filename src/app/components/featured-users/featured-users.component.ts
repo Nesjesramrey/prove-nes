ed2c6d113e522b8d3fb42905fc9e701eed9478fc
CATAAA233 <carlosdanielcatalan1o@gmail.com>
2022-08-27 22:35:21 -0700

    amount of visible users

--- src/app/components/featured-users/featured-users.component.ts
+++ src/app/components/featured-users/featured-users.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Console } from 'console';
 
 interface UserDataType{
   profileImage:string
@@ -10,6 +11,7 @@ interface UserDataType{
 })
 export class FeaturedUsersComponent implements OnInit {
 
+  @Input('inputAmount') amountOfVisibleUsers:number = 5;
   userData:UserDataType[] =[
     {
       profileImage:'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg',
@@ -36,9 +38,11 @@ export class FeaturedUsersComponent implements OnInit {
       profileImage:'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
   ]
-  visibleUsers:UserDataType[]= this.userData.slice(0,5);
+  visibleUsers:UserDataType[]= this.userData.slice(0,this.amountOfVisibleUsers);
   userQuantity:number = this.userData.length;
   constructor() { }
+
+  console = console.log(this.amountOfVisibleUsers);
 
   trackByFn(index:number, userData:UserDataType):number{
     return index;
