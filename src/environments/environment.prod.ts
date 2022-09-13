7bb311441556c98b3c18d089c9da8a0081c2988d
nenkyyy <nenkyyy@gmail.com>
2022-09-13 12:33:05 -0500

    ajuste de url de backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   version: "2022.9.7.1",
-  apiEndPoint: 'http://pando-backend-dev-env.eba-zk3eys8d.us-east-1.elasticbeanstalk.com/api/v1',
-  apiSocket: 'http://pando-backend-dev-env.eba-zk3eys8d.us-east-1.elasticbeanstalk.com'
+  apiEndPoint: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com/api/v1',
+  apiSocket: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com'
 };
