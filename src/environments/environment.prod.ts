cbc877c1dea70e764782a2e5e8e01020b79caa12
nenkyyy <nenkyyy@gmail.com>
2022-09-13 14:05:33 -0500

    cambiar version de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: "2022.9.7.1",
+  version: "2022.9.13.1",
   apiEndPoint: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com/api/v1',
   apiSocket: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com'
 };
