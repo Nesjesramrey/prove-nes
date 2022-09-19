dd450cab66b9b4d8a34337b011e0d4141df95037
Alexis Rangel <alexis.rangel@codster.io>
2022-09-19 22:09:32 +0000

    environment.prod.ts edited online with Bitbucket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   version: "2022.9.17.1",
-  apiEndPoint: 'https://pando-backend-prod-env.eba-nwmpnfza.us-east-1.elasticbeanstalk.com/api/v1',
-  apiSocket: 'https://pando-backend-prod-env.eba-nwmpnfza.us-east-1.elasticbeanstalk.com'
+  apiEndPoint: 'http://pando-backend-prod-env.eba-nwmpnfza.us-east-1.elasticbeanstalk.com/api/v1',
+  apiSocket: 'http://pando-backend-prod-env.eba-nwmpnfza.us-east-1.elasticbeanstalk.com'
 };
