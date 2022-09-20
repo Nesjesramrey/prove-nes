39b343c4bff568ef495afa2c1c79a6fc01e63b44
Alexis Rangel <alexis.rangel@codster.io>
2022-09-20 17:28:57 +0000

    environment.prod.ts edited online with Bitbucket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   version: "2022.9.17.1",
-  apiEndPoint: 'http://pando-backend-prod-env.eba-nwmpnfza.us-east-1.elasticbeanstalk.com/api/v1',
-  apiSocket: 'http://pando-backend-prod-env.eba-nwmpnfza.us-east-1.elasticbeanstalk.com'
+  apiEndPoint: 'https://webservice.mexicolectivo.com/api/v1',
+  apiSocket: 'https://webservice.mexicolectivo.com'
 };
