0181745d6bb936083c80ec82485d4199a68b903d
nenkyyy <nenkyyy@gmail.com>
2022-10-11 14:04:08 -0500

    fix backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   version: "2022.10.7.1",
-  apiEndPoint: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com/api/v1',
-  apiSocket: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com'
+  // apiEndPoint: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com/api/v1',
+  // apiSocket: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com'
+  apiEndPoint: 'https://webservice.mexicolectivo.com/api/v1',
+  apiSocket: 'https://webservice.mexicolectivo.com/'
 };
