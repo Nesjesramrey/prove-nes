fc770e1916ff39f25f341e32b7391696acf7f4c0
nenkyyy <nenkyyy@gmail.com>
2022-10-20 15:24:47 -0500

    front end markup fixes

--- src/app/public-documents/components/solutions-card/solutions-card.component.ts
+++ src/app/public-documents/components/solutions-card/solutions-card.component.ts
@@ -1,10 +1,8 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UtilityService } from 'src/app/services/utility.service';
-import { MatSort, Sort } from '@angular/material/sort';
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { CustomMatDataSource } from '../../custom-class/custom-table.component';
+import { MatSort } from '@angular/material/sort';
+
 @Component({
   selector: 'solutions-card',
   templateUrl: './solutions-card.component.html',
@@ -16,22 +14,22 @@ export class SolutionsCardComponent implements OnInit {
   public subcategoryID: string = '';
   public topicID: string = '';
   public displayedColumns: string[] = ['title', 'stats.score'];
-
   @Input() data: any = [];
   @ViewChild(MatSort) sort: MatSort = new MatSort();
+  @Output() public sendSolutionData = new EventEmitter<any>();
+
   constructor(
     private utilityService: UtilityService,
-    public activatedRoute: ActivatedRoute,
-    private _liveAnnouncer: LiveAnnouncer
+    public activatedRoute: ActivatedRoute
   ) {
     this.documentID = this.activatedRoute['snapshot']['params']['documentID'];
     this.categoryID = this.activatedRoute['snapshot']['params']['categoryID'];
-    this.subcategoryID =
-      this.activatedRoute['snapshot']['params']['subcategoryID'];
+    this.subcategoryID = this.activatedRoute['snapshot']['params']['subcategoryID'];
     this.topicID = this.activatedRoute['snapshot']['params']['topicID'];
   }
 
   ngOnInit(): void {
+    // console.log(this.data);
   }
 
   ngAfterViewInit() {
@@ -46,5 +44,9 @@ export class SolutionsCardComponent implements OnInit {
   applyFilterSolution(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.data.filter = filterValue.trim().toLowerCase();
+  }
+
+  addFirstSolution() {
+    this.sendSolutionData.emit({ add: true });
   }
 }
