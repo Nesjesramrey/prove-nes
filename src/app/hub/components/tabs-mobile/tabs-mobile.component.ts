17821df6db0fc01953ce838f7fe1187b7b07b59d
nestorramirez <nestor.ramirez@codster.io>
2022-12-16 12:43:07 -0600

    add tab component

---
+++ src/app/hub/components/tabs-mobile/tabs-mobile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-mobile',
+  templateUrl: './tabs-mobile.component.html',
+  styleUrls: ['./tabs-mobile.component.scss']
+})
+export class TabsMobileComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
