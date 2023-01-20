ccb7f3c94cf448037dd4974390772dbd6749c68c
nestorramirez <nestor.ramirez@codster.io>
2023-01-20 16:20:13 -0600

    fix update profile

--- src/app/hub/app-configuration/app-configuration.component.ts
+++ src/app/hub/app-configuration/app-configuration.component.ts
@@ -51,16 +51,13 @@ export class AppConfigurationComponent implements OnInit {
     public formBuilder: FormBuilder,
     public dialogData: MatDialog,
     public dialog: LyDialog,
-    public utilitySrvc: UtilityService,
+    public utilitySrvc: UtilityService
   ) {
     this.accessToken = this.authenticationSrvc.fetchAccessToken;
     this.isMobile = this.deviceDetectorService.isMobile();
   }
 
   ngOnInit(): void {
-
-    
-
     this.userService.fetchFireUser().subscribe({
       error: (error) => {
         switch (error['status']) {
@@ -68,12 +65,19 @@ export class AppConfigurationComponent implements OnInit {
       },
       next: (reply: any) => {
         this.user = reply;
-        //console.log(this.user);
-        this.happyArray = JSON.parse(this.user.associationInterests)
-        this.unhappyArray = JSON.parse(this.user.uninterestingTopics)
-        //console.log(this.user.uninterestingTopics)
+        if ((this.user.associationInterests == null)) {
+          console.log('error')
+          this.happyArray = [];
+        } else {
+    
+          this.happyArray = JSON.parse(this.user.associationInterests);
+        }
+        if ((this.user.uninterestingTopics == null )) {
+          this.unhappyArray = [];
+        } else {
+          this.unhappyArray = JSON.parse(this.user.uninterestingTopics);
+        }
 
-       
         this.formGroup = this.formBuilder.group({
           firstname: [this.user['firstname'], [Validators.required]],
           lastname: [this.user['lastname'], [Validators.required]],
@@ -118,7 +122,7 @@ export class AppConfigurationComponent implements OnInit {
       uninterestingTopics: JSON.stringify(this.unhappyArray) || null,
       associationInterests: JSON.stringify(this.happyArray) || null,
     };
-    
+
     this.userService.updateProfile(data).subscribe({
       error: (error) => {
         switch (error['status']) {
@@ -166,13 +170,13 @@ export class AppConfigurationComponent implements OnInit {
   }
 
   openDialogAssociationRegister() {
-      this.dialogData.open(AssociationRegisterComponent, {
+    this.dialogData.open(AssociationRegisterComponent, {
       data: {},
       height: '100%',
       maxWidth: '100%',
       panelClass: 'full-dialog',
     });
-    this.checked = !this.checked
+    this.checked = !this.checked;
   }
 
   clearForm() {
@@ -180,13 +184,11 @@ export class AppConfigurationComponent implements OnInit {
     this.formGroup.controls['gender'].reset();
   }
 
-  clearInputUnhappy(){
+  clearInputUnhappy() {
     this.unhappyArray = [];
   }
 
-  clearInputHappy(){
+  clearInputHappy() {
     this.happyArray = [];
   }
-
-
 }
