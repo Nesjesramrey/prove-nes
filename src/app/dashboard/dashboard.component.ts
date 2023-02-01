26fdfad8f175af0af8e54a52db61c830d5fa2265
nenkyyy <nenkyyy@gmail.com>
2023-02-01 12:57:26 -0600

    add slider button

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -83,7 +83,7 @@ export class DashboardComponent implements OnInit {
   }
 
   popPDF() {
-    window.open('https://static-assets-pando.s3.amazonaws.com/assets/200123_MxC_DIGITAL.pdf');
+    window.open('https://static-assets-pando.s3.amazonaws.com/assets/punto-de-partida.pdf');
   }
 
   moveSlide(index: number) {
