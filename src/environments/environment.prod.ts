45ae58e65dec68084d152a3e94bfdaa77b1571b9
nenkyyy <nenkyyy@gmail.com>
2023-02-24 12:08:26 -0600

    fix comment input

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: "2023.02.23.1",
+  version: "2023.02.24.1",
   // dev
   apiEndPoint: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com/api/v1',
   apiSocket: 'http://pando-backend-dev-env-1.us-east-1.elasticbeanstalk.com'
